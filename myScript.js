794d2cae2a8be8c8fe2f30b9ec888ae7bb80e726
Moses Palacios <mospalacios@gmail.com>
2019-12-10 11:05:02 -0500

    created color conditional statements and function to get locally saved data

--- myScript.js
+++ myScript.js
@@ -8,18 +8,32 @@ var hourArray = [{hours : 9 },
                 {hours : 16},
                 {hours : 17}];
 
-var todoList = []
+var todoList = [{activity: "test"},
+                {activity: ""},
+                {activity: ""},
+                {activity: "test"},
+                {activity: ""},
+                {activity: ""},
+                {activity: ""},
+                {activity: ""},
+                {activity: ""}]
 
 $(document).ready(function() {
 
-    
+
 
 //  $("#date").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
 
 
 // console.log(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
 
+//function for main date text
+function update() {
+    $("#date").text(moment().format('MMMM Do YYYY, h:mm:ss a'))
+};
 
+
+//dynamically set hour divs
 function showTimes() {
 
     for(i = 0; i < hourArray.length; i++) {
@@ -30,16 +44,16 @@ function showTimes() {
     var newChildDiv = $("<div>");
     newChildDiv.addClass("input-group mb-2");
     var prependDiv = $("<div>");
-    prependDiv.addClass("input-group-prepend");
+    prependDiv.addClass("col-2");
     var timeSpan = $("<span>");
     timeSpan.addClass("input-group-text bg-white border-left-0 border-danger rounded-0");
-    timeSpan.text(moment(hourArray[i]).format("dddd, hA")) ;
+    timeSpan.text(moment(hourArray[i]).format("h a")) ;
     var inputText = $("<input>");
-    inputText.addClass("form-control form-control-lg");
+    inputText.addClass("form-control form-control-lg activity");
     inputText.attr("type", "text");
-    inputText.attr("style", "background-color: lightgray");
+    inputText.attr("data-id", i);
     var appendDiv = $("<div>");
-    appendDiv.addClass("input-group-append");
+    appendDiv.addClass("col-2 text-center");
     var saveSpan = $("<span>");
     saveSpan.addClass("input-group-text btn");
     saveSpan.text("Save") ;
@@ -57,21 +71,79 @@ function showTimes() {
 
 };
 
-function update() {
-    $("#date").text(moment().format('MMMM Do YYYY, h:mm:ss a'))
-};
+//local storage functions
+function getLocal(){
+    var storedTodo = JSON.parse(localStorage.getItem("todoList"));
 
-function saveLocal(){
+    if(storedTodo !== null){
+        todoList = storedTodo
+        } else {
+            todoList
+        };
+$(".activity").each(function(thing){
+    var activity = todoList[thing].activity;
+    $(this).val(activity)
+    console.log($(this).attr("data-id"))
+    })
 
 }
 
+function saveLocal(){
+    localStorage.setItem("todoList", JSON.stringify(todoList));
+}
 
+
+//run on start
 setInterval(update, 1000);
 
 showTimes();
+getLocal();
+console.log(JSON.parse(localStorage.getItem("todoList")));
+console.log(moment().toObject());
 
-console.log(moment().toObject())
 
+
+
+
+//color conditionals
+// set({"hour": 12, "minute" : 30})
+
+
+    $(".activity").each(function(hour){
+
+        if(moment().isBetween(hourArray[hour], hourArray[hour + 1]) || moment().isSame(hourArray[hour])) {
+            console.log("true");
+            console.log(this);
+            $(this).attr("style",  "background-color: pink")
+            
+    } else if(moment().isBefore(hourArray[hour])) {
+        console.log("false");
+        $(this).attr("style",  "background-color: green")
+        
+    } else {
+        $(this).attr("style",  "background-color: lightblue") 
+    }
+        
+    
+
+
+    })
+    
+    console.log(hourArray[3], hourArray[3 + 1]);
+    console.log(moment().hour())
+
+
+//click events
+
+
+
+
+
+
+
+
+// saveLocal();
+// getLocal();
 
 
 
